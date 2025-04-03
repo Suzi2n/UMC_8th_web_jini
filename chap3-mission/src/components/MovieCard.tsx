32df10d7700afdd8322dc9dbefc277d45e0d5896
Suzi2n <jin23568@naver.com>
2025-04-03 14:36:23 +0900

    chap3-mission

--- chap3-mission/src/components/MovieCard.tsx
+++ chap3-mission/src/components/MovieCard.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import {Movie} from '../types/movie'
-import { useNavigate } from 'react-router-dom';
 
 interface MovieCardProps {
     movie: Movie;
@@ -8,12 +7,9 @@ interface MovieCardProps {
 
 export default function MovieCard({movie} : MovieCardProps) {
     const [isHovered, setIsHovered] = useState(false);
-    const navigate = useNavigate();
-  
 
     return (
     <div 
-        onClick={(): void | Promise<void> => navigate(`/movie/${movie.id}`)}
         className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer w-44 transition-transform duration-300 hover:scale-105'
         onMouseEnter={(): void => setIsHovered(true)}
         onMouseLeave={(): void => setIsHovered(false)}
