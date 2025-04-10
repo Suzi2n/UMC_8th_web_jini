ca435d368ade28f3da6b7f10de03a7ae85746324
Suzi2n <jin23568@naver.com>
2025-04-11 00:28:09 +0900

    chap4-mission3

---
+++ chap4-mission3/src/pages/Mypage.tsx
@@ -0,0 +1,21 @@
+import { useEffect , useState} from 'react'
+import { getMyInfo } from '../apis/auth';
+import { ResponseMyInfoDto } from '../types/auth';
+
+const Mypage = () => {
+    const [data, setData] = useState<ResponseMyInfoDto| null>(null);
+    useEffect(() => {
+        const getData = async () => {
+            const response = await getMyInfo();
+            console.log(response);
+
+            setData(response);
+        };
+        getData();
+    }, []);
+    console.log(data?.data?.name);
+    return <div>{data?.data?.name}</div>;
+
+}
+
+export default Mypage;
