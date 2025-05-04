b7050c992391d98129e1dc63cfc68f43bf59cfed
Suzi2n <jin23568@naver.com>
2025-05-04 20:29:40 +0900

    chap5-mission2

---
+++ chap5-mission2/src/App.tsx
@@ -0,0 +1,56 @@
+
+import './App.css'
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import HomeLayout from './layouts/HomeLayout';
+import Mypage from './pages/Mypage';
+import { AuthProvider } from './context/AuthContext';
+import ProtextedLayout from './layouts/ProtectedLayout';
+
+
+// publicRoutes: 인증 없이 접근 가능한 라우트
+const publicRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {index: true, element: <HomePage />},
+      {path: 'login', element: <LoginPage />},
+      {path: 'signup', element: <SignUpPage />},
+    ],
+  },
+];
+
+// protextedRoutes: 인증이 필요한 라우트
+
+const protextedRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <ProtextedLayout/>,
+    errorElement: <NotFoundPage />,
+    children: [
+      {path: 'my', element: <Mypage/>},
+    ],
+
+  },
+]
+
+
+const router = createBrowserRouter([...publicRoutes, ...protextedRoutes]);
+
+
+function App() {
+
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+
+}
+
+export default App;
