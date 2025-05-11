956c08f0268c93ca6eb8ea4c9770ed8b2cad090b
Suzi2n <jin23568@naver.com>
2025-05-11 18:52:50 +0900

    chap6-mission1

---
+++ chap6-mission1/src/types/common.ts
@@ -0,0 +1,27 @@
+export type CommonResponse<T> = {
+    status: boolean;
+    statusCode: number;
+    message: string;
+    data: T;
+
+}
+
+export type CursorBasedResponse<T> = {
+    status: boolean;
+    statusCode: number;
+    message: string;
+    data: T;
+    nextCursor: number;
+    hasNext: boolean;
+}
+
+enum PAGINATION_ORDER {
+    "asc" = 'asc',
+    "desc" = 'desc',
+}
+export type PaginationDto = {
+    cursor?: number;
+    limit?: number;
+    search?: string;
+    order?: PAGINATION_ORDER;
+}
